ac1671086052a86676a616a76dfd51bc63060e67
Tyler <tyler@future-ink.com>
2015-09-10 14:01:16 -0700

    first commit

---
+++ provision.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+
+# Use single quotes instead of double quotes to make it work with special-character passwords
+PASSWORD='root'
+
+echo "-------------------------------"
+echo "--- Creating Project Folder ---"
+echo "-------------------------------"
+sudo mkdir "/var/www"
+
+echo "------------------------"
+echo "--- Update / Upgrade ---"
+echo "------------------------"
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+echo "-------------------------------"
+echo "--- Installing Apache & PHP ---"
+echo "-------------------------------"
+sudo apt-get install -y apache2
+sudo apt-get install -y php5
+
+echo "-------------------------------"
+echo "--- Installing MySQL Server ---"
+echo "-------------------------------"
+sudo debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password $PASSWORD"
+sudo debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password $PASSWORD"
+sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
+
+echo "-----------------------------------"
+echo "--- Installing PHP Dependencies ---"
+echo "-----------------------------------"
+sudo apt-get -y install libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-mysql git-core
+sudo apt-get update
+
+echo "----------------------------------------------------------------"
+echo "--- Installing PHPMyAdmin: $PASSWORD/$PASSWORD ---"
+echo "----------------------------------------------------------------"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+sudo apt-get -y install phpmyadmin
+
+echo "-----------------------------------"
+echo "--- Setup Hosts ---"
+echo "-----------------------------------"
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+    DocumentRoot "/var/www"
+    <Directory "/var/www">
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+
+echo "----------------------------"
+echo "--- Enabling mod_rewrite ---"
+echo "----------------------------"
+sudo a2enmod rewrite
+
+echo "----------------------"
+echo "--- Turn on errors ---"
+echo "----------------------"
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
+
+sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+
+echo "--------------------------"
+echo "--- Restarting Apache: ---"
+echo "--------------------------"
+sudo service apache2 restart
+
+echo "-----------------------------"
+echo "--- Installing Ruby 1.9.3 ---"
+echo "-----------------------------"
+sudo apt-get update
+sudo apt-get install -y ruby1.9.3
+
+echo "-----------------------"
+echo "--- Installing Git: ---"
+echo "-----------------------"
+sudo apt-get update
+sudo apt-get -y install git
+
+echo "--------------------------"
+echo "--- Installing Node ---"
+echo "--------------------------"
+sudo apt-get update
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install -y nodejs
+
+echo "----------------------------"
+echo "--- Installing Composer: ---"
+echo "----------------------------"
+curl -s https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+echo "Project setup complete..."
+echo "Type 'vagrant ssh && cd /var' to log into the machine."
